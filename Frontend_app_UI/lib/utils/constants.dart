b4a7774aeb58e2ee1ebd24c264d268d5e83c30a1
PR-Skandha-B <skandha.bharadwaj.75@mail.com>
2026-06-05 08:48:40 +0530

    Updated Frontend with live Railway API URL-1

--- Frontend_app_UI/lib/utils/constants.dart
+++ Frontend_app_UI/lib/utils/constants.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/foundation.dart';
 
 class AppConstants {
-  static const String baseUrl = 'livechatapp-production-dc3a.up.railway.app';
-  static const String socketUrl = 'livechatapp-production-dc3a.up.railway.app';
+  static const String baseUrl = 'https://livechatapp-production-dc3a.up.railway.app';
+  static const String socketUrl = 'https://livechatapp-production-dc3a.up.railway.app';
   static const String tokenKey = 'auth_token';
   static const String userKey = 'user_data';
 }
